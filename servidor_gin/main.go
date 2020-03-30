67f1ae39f4527513cddef78e9921db0c81fc8116
Cristian Sanchez <cristians@lagash.com>
2020-03-30 13:12:28 -0500

    Creación de endpoint ping con respuesta pong

---
+++ servidor_gin/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()                    // crear una instancia de gin-lo que sera el servidor (servidor en memoria)
+	r.GET("/ping", func(c *gin.Context) { // metodo http llamada Get (traer) recibe dos parametros un string y una funcion
+		c.JSON(200, gin.H{ //funcion JSON // retorna status code
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080") levanta el servidor
+}
