3a696b662db390118a263627d568196ff3f7d8c0
Cristian Sanchez <cristians@lagash.com>
2020-03-30 13:46:21 -0500

    tarea 14

--- servidor_gin/main.go
+++ servidor_gin/main.go
@@ -6,7 +6,7 @@ func main() {
 	r := gin.Default()                    // crear una instancia de gin-lo que sera el servidor (servidor en memoria)
 	r.GET("/ping", func(c *gin.Context) { // metodo http llamada Get (traer) recibe dos parametros un string y una funcion
 		c.JSON(200, gin.H{ //funcion JSON // retorna status code
-			"message": "pong",
+			"message": "pong", // esto es lo que debe responder el api
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080") levanta el servidor
